a41225dafbb2b2b71eacbe92e40754fe9888a701
100siha <100siha@gmail.com>
2026-03-31 12:28:16 +0900

    NumberGame Code

--- Assets/CStudy/EnemyController.cs
+++ Assets/CStudy/EnemyController.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    GameManager gameMgrObject;
+
     void Start()
     {
-        
+        TakeDamage(10);
+        Debug.Log("state : ");
     }
 
-    void Update()
+    public void TakeDamage(int damage)
     {
-        
+        Debug.Log("damage " + damage);
+        return;
     }
-}
+} 
